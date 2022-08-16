8294d1d118f04be91396ccf93c266e7ace72d02f
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-08-16 05:50:07 -0300

    feat: new changes in code fro

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Rafael Ribeiro",
   title: "Hello there, I'm Rafael",
   subTitle: emoji(
-    "A passionate Software Engineer 🚀 having an experience in solving problems and building applications with Node.JS / Python / Go / Java. I turn ☕ Coffee into Code."
+    "A passionate Software Engineer 🚀 having an experience in solving problems and building applications with Node.js / Python / Go / Java. I turn ☕ Coffee into Code."
   ),
   resumeLink:
     "https://drive.google.com/file/d/18yUS_SVRT9IBEKr2oMhVNzWEoFs2PZdo/view?usp=sharing", // Set to empty to hide the button
@@ -49,13 +49,14 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle:
-    "Software Engineer who loves to solve problems through technology.",
+  subTitle: "Software Engineer who loves to solve problems through technology.",
   skills: [
     emoji(
       "👷‍♂️ Software Engineering - I love to design, build and deploy applications with the best technologies available."
     ),
-    emoji("🤖 Project experience with Machine Learning projects, data analysis and visualization, and Deep Learning."),
+    emoji(
+      "🤖 Project experience with Machine Learning projects, data analysis and visualization, and Deep Learning."
+    ),
     emoji(
       "🔄 Full Cycle Software Development - I have a strong background in software development, and deploy/integration of third party services such as Firebase/ AWS /Heroku."
     ),
@@ -145,7 +146,7 @@ const educationInfo = {
       subHeader: "Bachelor in Computational Physics",
       duration: "March 2013 - December 2018",
       desc: "Participated in the research Higgs Boson.",
-      descBullets: ["Teacher Assistant in Calculus I and Physics II",]
+      descBullets: ["Teacher Assistant in Calculus I and Physics II"]
     }
   ]
 };
@@ -187,8 +188,7 @@ const workExperiences = {
         "Develop Combinatorial optimization algorithms",
         "Software Development to create solutions based on data"
       ]
-    },
-    
+    }
   ]
 };
 
@@ -251,8 +251,7 @@ const achievementSection = {
         {
           name: "Certification",
           url: "https://www.coursera.org/account/accomplishments/specialization/certificate/JUVVH9QG26VE"
-        },
-       
+        }
       ]
     },
     {
@@ -264,8 +263,7 @@ const achievementSection = {
         {
           name: "Certification",
           url: "https://www.coursera.org/account/accomplishments/specialization/certificate/SYTLRYME4GZQ"
-        },
-       
+        }
       ]
     },
     {
@@ -277,11 +275,10 @@ const achievementSection = {
         {
           name: "Certification",
           url: "https://www.coursera.org/account/accomplishments/specialization/certificate/Z8PU6AT9Z657"
-        },
-       
+        }
       ]
     },
-    
+
     {
       title: "Machine Learning on Google Cloud Platform",
       subtitle:
@@ -291,6 +288,30 @@ const achievementSection = {
         {
           name: "Certification",
           url: "https://www.coursera.org/account/accomplishments/specialization/certificate/2CXZRBURD3EC"
+        }
+      ]
+    },
+    // {
+    //   title: "Big Data on Google Cloud Platform",
+    //   subtitle:
+    //     "Nesta especialização on-line intensiva de cinco semanas, os participantes terão uma introdução prática sobre como projetar e criar sistemas de processamento de dados no Google Cloud Platform. Por meio de uma combinação de apresentações, demonstrações e laboratórios práticos, os participantes aprenderão a projetar sistemas de processamento de dados, criar canais completos e análises de dados e desenvolver soluções de aprendizado de máquina. Neste curso, abordamos dados estruturados, não estruturados e de streaming",
+    //   image: require("./assets/images/google_logo.png"),
+    //   footerLink: [
+    //     {
+    //       name: "Certification",
+    //       url: "https://www.coursera.org/account/accomplishments/certificate/MLWMETXKJ8X8"
+    //     }
+    //   ]
+    //},
+    {
+      title: "Bayesian Methods for Machine Learning",
+      subtitle:
+        "People apply Bayesian methods in many areas: from game development to drug discovery. They give superpowers to many machine learning algorithms: handling missing data, extracting much more information from small datasets. Bayesian methods also allow us to estimate uncertainty in predictions, which is a desirable feature for fields like medicine. ",
+      image: require("./assets/images/hse_logo.png"),
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://www.coursera.org/account/accomplishments/certificate/PD7P4XHQRMNW"
         }
       ]
     },
@@ -330,50 +351,78 @@ const achievementSection = {
         }
       ]
     },
+    // {
+    //   title: "PostgreSQL for Everybody",
+    //   subtitle:
+    //     "Across these four courses, you’ll learn how to use the PostgreSQL database and explore topics ranging from database design to database architecture and deployment. You’ll also compare and contrast SQL and NoSQL approaches to database design. The skills in this course will be useful to learners doing data mining or application development.",
+    //   image: require("./assets/images/michigan_logo.png"),
+    //   footerLink: [
+    //     {
+    //       name: "Certification",
+    //       url: "https://www.coursera.org/account/accomplishments/certificate/YL3P4PWPLBT9"
+    //     }
+    //   ]
+    // },
+    // {
+    //   title: "Programming with Google Go",
+    //   subtitle:
+    //     "This specialization introduces the Go programming language from Google and provides learners with an overview of Go's special features. Upon completing the three-course sequence, learners will have gained the knowledge and skills needed to create concise, efficient, and clean applications using Go.",
+    //   image: require("./assets/images/university-of-california-irvine-uci-vector-logo.png"),
+    //   footerLink: [
+    //     {
+    //       name: "Certification",
+    //       url: "https://www.coursera.org/account/accomplishments/certificate/YL3P4PWPLBT9"
+    //     }
+    //   ]
+    // },
     {
-      title: "PostgreSQL for Everybody",
-      subtitle:"Across these four courses, you’ll learn how to use the PostgreSQL database and explore topics ranging from database design to database architecture and deployment. You’ll also compare and contrast SQL and NoSQL approaches to database design. The skills in this course will be useful to learners doing data mining or application development.",
-      image: require("./assets/images/michigan_logo.png"),
-      footerLink: [
-        {
-          name: "Certification",
-          url: "https://www.coursera.org/account/accomplishments/certificate/YL3P4PWPLBT9",
-        }
-      ]
-    },
-    {
-      title: "Programming with Google Go",
-      subtitle:"This specialization introduces the Go programming language from Google and provides learners with an overview of Go's special features. Upon completing the three-course sequence, learners will have gained the knowledge and skills needed to create concise, efficient, and clean applications using Go.",
-      image: require("./assets/images/university-of-california-irvine-uci-vector-logo.png"),
-      footerLink: [
-        {
-          name: "Certification",
-          url: "https://www.coursera.org/account/accomplishments/certificate/YL3P4PWPLBT9",
-        }
-      ]
-    },{
       title: "Node.js Ignite",
-      subtitle:"Build scalable and simple architectures for the web using a flexible and popular language.",
+      subtitle:
+        "Build scalable and simple architectures for the web using a flexible and popular language.",
       image: require("./assets/images/28929274.png"),
       footerLink: [
         {
           name: "Certification",
-          url: "https://app.rocketseat.com.br/api/certificates/pdf/ef1bad6b-937a-49f9-a6f3-421a4fda7d99",
+          url: "https://app.rocketseat.com.br/api/certificates/pdf/ef1bad6b-937a-49f9-a6f3-421a4fda7d99"
         }
       ]
     },
     {
       title: "AWS Machine Learning Foundations",
-      subtitle:"Learn the fundamentals of advanced machine learning areas such as computer vision, reinforcement learning, and generative AI. Get hands-on with machine learning using AWS AI Devices (i.e. AWS DeepRacer and AWS DeepComposer).Learn how to prepare, build, train, and deploy high-quality machine learning (ML) models quickly with Amazon SageMaker and learn object-oriented programming best practices.",
+      subtitle:
+        "Learn the fundamentals of advanced machine learning areas such as computer vision, reinforcement learning, and generative AI. Get hands-on with machine learning using AWS AI Devices (i.e. AWS DeepRacer and AWS DeepComposer).Learn how to prepare, build, train, and deploy high-quality machine learning (ML) models quickly with Amazon SageMaker and learn object-oriented programming best practices.",
       image: require("./assets/images/udacity.png"),
       footerLink: [
         {
           name: "Certification",
-          url: "https://app.rocketseat.com.br/api/certificates/pdf/ef1bad6b-937a-49f9-a6f3-421a4fda7d99",
+          url: "https://app.rocketseat.com.br/api/certificates/pdf/ef1bad6b-937a-49f9-a6f3-421a4fda7d99"
         }
       ]
     },
-
+    {
+      title: "Backend with Javascript",
+      subtitle:
+        "JavaScript is now one of the most popular programming languages ​​on the web and a few years ago it was no longer used only in browsers, in order to be able to assign behaviors to pages. Now it also has space in the development of back-end applications, mobile and web apps, in addition to games. This training is suitable for those who are just starting in programming and chose JavaScript as the first language to delve into and the backend as a branch of web development to work on. Let's start by covering the fundamental parts of any programming language (data types, functions, arrays and objects) and how to work with them with JavaScript.",
+      image: require("./assets/images/alura.jpeg"),
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://cursos.alura.com.br/degree/certificate/928d70b4-dc47-49da-97b5-eb6af0df95ed"
+        }
+      ]
+    },
+    {
+      title: "Go: Google's programming language",
+      subtitle:
+        "The Go language, also known as GoLang, is an open source language that was created by Google in 2007, and has since been used to build large-scale products and services. Currently, the language is used by several companies, such as Uber, Twitch, Medium and Mercado Livre. Go is a simple and productive language to use, focused on the development of applications that require high performance.",
+      image: require("./assets/images/alura.jpeg"),
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://cursos.alura.com.br/degree/certificate/d977171b-b19d-4f37-83c5-6b66148e3f68"
+        }
+      ]
+    }
   ],
   display: true // Set false to hide this section, defaults to true
 };
